9c707777b796ea3786318f71aa61c4c634b58903
Kerwin joel <joelss4029@gmail.com>
2021-09-27 13:39:57 -0500

    config ready to init in react

---
+++ src/components/HelloWorld.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import '../assets/styles/App.scss'
+export const HelloWorld = () => {
+    return (
+        <div>
+            <h1>hola mundo desde el componente jelou</h1>
+        </div>
+    )
+}
